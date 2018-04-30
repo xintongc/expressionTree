a8175e52d6b04198a022b976c956ba2f296a6885
zncu <xintongc@gmail.com>
2018-04-29 22:13:09 -0400

    add class

--- main.cpp
+++ main.cpp
@@ -87,31 +87,31 @@ int calculate(string string){
 
 int main() {
 
-    ComponentNode *leaf5 = new LeafNode(5);
-    ComponentNode *leaf4 = new LeafNode(4);
-    ComponentNode *leaf3 = new LeafNode(3);
-
-    ComponentNode *add = new AddNode(leaf3,leaf4);
-    ComponentNode *negate = new NegateNode(leaf5);
-    ComponentNode *mul = new MultiplyNode(negate,add);
-
-    double result = mul->calculate();
-    cout << result << endl;
-
-    ComponentNode *leaf1 = new LeafNode(1);
-    ComponentNode *leaf2 = new LeafNode(2);
- //   ComponentNode * div = new DivideNode(negate,add);
-    ComponentNode * div = new DivideNode(leaf2,leaf1);
-    double result2 = div->calculate();
-    cout << result2;
-
-    delete leaf5;
-    delete leaf4;
-    delete leaf3;
-    delete add;
-    delete negate;
-    delete mul;
-    delete div;
+//    ComponentNode *leaf5 = new LeafNode(5);
+//    ComponentNode *leaf4 = new LeafNode(4);
+//    ComponentNode *leaf3 = new LeafNode(3);
+//
+//    ComponentNode *add = new AddNode(leaf3,leaf4);
+//    ComponentNode *negate = new NegateNode(leaf5);
+//    ComponentNode *mul = new MultiplyNode(negate,add);
+//
+//    double result = mul->calculate();
+//    cout << result << endl;
+//
+//    ComponentNode *leaf1 = new LeafNode(1);
+//    ComponentNode *leaf2 = new LeafNode(2);
+// //   ComponentNode * div = new DivideNode(negate,add);
+//    ComponentNode * div = new DivideNode(leaf2,leaf1);
+//    double result2 = div->calculate();
+//    cout << result2;
+//
+//    delete leaf5;
+//    delete leaf4;
+//    delete leaf3;
+//    delete add;
+//    delete negate;
+//    delete mul;
+//    delete div;
 
 
     cout << calculate("3+2*5-6+5*3");
